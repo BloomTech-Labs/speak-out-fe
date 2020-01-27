c4ba5f6b87e72fc4c55428d94e4923af82951073
Dave Vazquez <dmvaz.li@gmail.com>
2020-01-27 17:14:08 -0500

    demo commit

--- client/src/index.js
+++ client/src/index.js
@@ -1,26 +1,26 @@
+import './index.css';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import thunk from 'redux-thunk';
-import { logger } from 'redux-logger';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import { BrowserRouter } from "react-router-dom";
-import { reducer } from "./reducers/index";
-// import 'semantic-ui-css/semantic.min.css';
-
-import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+import { applyMiddleware, createStore } from 'redux';
+import { logger } from 'redux-logger';
+import thunk from 'redux-thunk';
 import App from './App';
-
+import { reducer } from './reducers/index';
+// import 'semantic-ui-css/semantic.min.css';
 
 
 const store = createStore(reducer, applyMiddleware(thunk, logger));
 
 ReactDOM.render(
-    <Provider store={store} >
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
-    </Provider>, 
-    document.getElementById('root')
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById('root')
 );
 
+// comment
